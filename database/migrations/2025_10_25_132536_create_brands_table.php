93ea461006765619c5271f17872cf7565bd35d17
Jarren Dejito <jarrendehito@gmail.com>
2025-11-27 16:42:19 +0800

    Chapter2

--- database/migrations/2025_10_25_132536_create_brands_table.php
+++ database/migrations/2025_10_25_132536_create_brands_table.php
@@ -13,10 +13,6 @@ return new class extends Migration
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('slug')->unique();
-            $table->string('image')->nullable();
-            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
