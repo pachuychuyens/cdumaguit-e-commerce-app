47952fb81d5af3f76e70bbcb2a395f5523a5797c
Jarren Dejito <jarrendehito@gmail.com>
2025-12-04 01:55:19 +0800

    chapter16

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -30,6 +30,8 @@ class ProductsPage extends Component
     public $on_sale = [];
     #[Url]
     public $price_range = 300000;
+    #[Url]
+    public $sort = 'latest';
     public function render()
     {
         $productQuery = Product::query()->where('is_active', 1);
@@ -53,6 +55,14 @@ class ProductsPage extends Component
             {
                 $productQuery->whereBetween('price', [0,$this->price_range]);
             }
+            if($this->sort == 'latest')
+            {
+                $productQuery->latest();
+            }
+            if($this->sort == 'price')
+            {
+                $productQuery->orderBy('price');
+            }    
         return view('livewire.products-page', [
             'products' => $productQuery->paginate(9),
             'brands' => Brand::where('is_active', 1)->get(['id','name','slug']),
