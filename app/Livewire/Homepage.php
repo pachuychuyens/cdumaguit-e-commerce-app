241539a8963449be5520c6ecbc78a2ceaae92f2b
Jarren Dejito <jarrendehito@gmail.com>
2025-12-03 18:05:35 +0800

    chapter13

--- app/Livewire/Homepage.php
+++ app/Livewire/Homepage.php
@@ -3,11 +3,22 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Brand;
+use App\Models\Category;
+use Livewire\Attributes\Title;
+
+
+#[Title('Home Page - E-commerce')]
 
 class Homepage extends Component
 {
     public function render()
     {
-        return view('livewire.homepage');
+        $brands = Brand::where('is_active', 1)->get();
+        $categories = Category::where('is_active', 1)->get(); 
+        return view('livewire.home-page' ,[
+            'brands' => $brands,
+            'categories' => $categories
+        ]);
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
     
 
   ],
-  safelist: [
+  safelist: [ 
   'bg-blue-500',
   'bg-yellow-500',
   'bg-green-500',
