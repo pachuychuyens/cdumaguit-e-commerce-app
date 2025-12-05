b3b4c509dc553104428d2de0c0481a253c87d562
Jarren Dejito <jarrendehito@gmail.com>
2025-12-05 21:45:11 +0800

    chapter21

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -3,11 +3,41 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use App\Helpers\CartManagement;
 
+#[Title('Checkout')]
 class CheckoutPage extends Component
-{
+{   
+    public $first_name;
+    public $last_name;
+    public $phone;
+    public $street_address;
+    public $city;
+    public $state;
+    public $zip_code;
+    public $payment_method;
+    public function placeOrder()
+    {
+        $this->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'phone' => 'required',
+            'street_address' => 'required',
+            'city' => 'required',
+            'state' => 'required',
+            'zip_code' => 'required',
+            'payment_method' => 'required',
+            
+        ]);
+    }
     public function render()
     {
-        return view('livewire.checkout-page');
+        $cart_items = CartManagement::getCartItemsFromCookie();
+        $grand_total = CartManagement::calculateGrandTotal($cart_items);
+        return view('livewire.checkout-page',[
+            'cart_items' => $cart_items,
+            'grand_total' => $grand_total,
+        ]);
     }
 }
